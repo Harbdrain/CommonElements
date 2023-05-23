83a59c08419d77f615f3eb5b683b2d51dde839c2
Harbdrain <mail@d-demchenko.ru>
2023-05-23 19:50:06 +0300

    Done.

---
+++ src/main/java/com/danil/commonelements/App.java
@@ -0,0 +1,22 @@
+package com.danil.commonelements;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.TreeSet;
+
+public class App {
+    private static Collection<Integer> common(Collection<Integer> a, Collection<Integer> b) {
+        TreeSet<Integer> result = new TreeSet<>(a);
+        result.retainAll(b);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> a = new ArrayList<>(Arrays.asList(6, 4, 6, 8, 1, 100, -100));
+        List<Integer> b = new ArrayList<>(Arrays.asList(3, 8, 76544, -42, 100, 1));
+        Collection<Integer> result = common(a, b);
+        System.out.println(result);
+    }
+}
